e1aa7bec4009eea94a3ec4571c6d7c2dca072d37
mhassan <m.hassan_102@yahoo.com>
2017-08-08 16:03:24 +0500

    Initial commit 2

--- hello.py
+++ hello.py
@@ -1,2 +1,13 @@
+import platform
+print(platform.machine())
+print(platform.version())
+print(platform.platform())
+print(platform.uname())
+print(platform.system())
+print(platform.processor())
+
+
+
+
 print("Hello world from Travis CI")
 print("Hello world from Travis CI")
